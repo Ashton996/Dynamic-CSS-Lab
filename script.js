e015e86600f0748768b6dc583049249096930be4
Ashton996 <aramseur9620@student.clevelandcountyschools.org>
2024-12-12 09:03:17 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var small= 15;
+var medium= 30;
+var large= 60;
+
+function makeSmall()
+{
+document.getElementById("name").style.fontSize = small  + "px";
+
+  
+}
+
+function makeMedium()
+{
+  document.getElementById("name").style.fontSize = medium + "px";
+
+}
+
+function makeLarge()
+{
+  document.getElementById("name").style.fontSize = large + "px";
+
+}
+
